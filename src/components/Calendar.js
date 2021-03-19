3437130c3f805930d387d5009cbdbbfda7318148
mcardona9015 <mcardona9015@gmail.com>
2021-03-19 14:01:17 -0400

    added calendar event adition

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,50 +3,74 @@
 import Scheduler from "devextreme-react/scheduler";
 import "devextreme/dist/css/dx.common.css";
 import "devextreme/dist/css/dx.light.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/Calendar.css";
-
-// function Calendar() {
-//   //   return <Scheduler id="scheduler">{/* Configuration goes here */}</Scheduler>;
-//   return <div>Calendar</div>;
-// }
-
-// export default Calendar;
-
-import { data } from "./data.js";
-
-const views = ["day", "week", "month"];
 
 function Calendar() {
   const [currentDate, setCurrentDate] = useState(new Date());
-  console.log("currentDate: ", currentDate);
+  const [calendarEvents, setCalendarEvents] = useState(null);
+  const url = "http://localhost:3000/calendar";
 
-  function handleClick(e) {
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      fetch(url, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then(setCalendarEvents)
+        .then(console.log("activated"));
+    }
+  }, []);
+
+  function handleCellClick(e) {
     setCurrentDate(e.cellData.startDate);
   }
 
   function handleAppointmentAdd(e) {
     console.log("e: ", e);
+    const appointmentData = e.appointmentData;
+    console.log("appointmentData: ", appointmentData);
+    const token = localStorage.getItem("token");
+    if (token) {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(appointmentData),
+      })
+        .then((response) => response.json())
+        .then(console.log);
+    }
+  }
+
+  function handleCurrentDayChange(e) {
+    setCurrentDate(e);
   }
 
   return (
     <section className="calendar-container">
       <Scheduler
         onAppointmentAdded={handleAppointmentAdd}
-        onCellClick={handleClick}
+        onCellClick={handleCellClick}
         className="calendar-month"
         timeZone="America/New_York"
-        dataSource={data}
-        views={views}
+        dataSource={calendarEvents}
+        views={["week", "month"]}
         defaultCurrentView="month"
         defaultCurrentDate={currentDate}
         height={800}
         startDayHour={9}
       />
       <Scheduler
+        onCurrentDateChange={handleCurrentDayChange}
         className="calendar-day"
         timeZone="America/New_York"
-        dataSource={data}
+        dataSource={calendarEvents}
         views={["day"]}
         currentDate={currentDate}
         height={800}
